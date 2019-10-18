960944f78e221c9e5702f87d3bf4ab255e837579
McCaffreyCarson <carson@coulstontech.net>
2019-10-18 17:22:53 -0400

    time fix

--- app.py
+++ app.py
@@ -19,16 +19,10 @@ def msg_received_from_group():
   log('{}'.format(data))
   
   #Check the text of the message sent to the chat to see if it matches our command word
-  if data['text'].lower() == "!test":
-    send_msg("Hello World!")
 
   if data['text'].lower() == "!check":
-    send_msg("It is " + str(time.localtime) + " and Michigan still sucks!")
-	
-
-  elif data['text'].lower() == "!testpic":
-    send_msg_pic("Hello World!","https://i.groupme.com/1024x1024.jpeg.d733d6de5c36462f8d1cb67e3191b618")
-	
+    seconds = time.time()
+    send_msg("It is " + str(time.ctime(seconds)) + " and Michigan still sucks!")
 	
 
   return "ok", 200
